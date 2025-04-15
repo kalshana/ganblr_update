9ab36cd811962a38eef56913217d0250ee98f583
kalshana <kalshana@deakin.edu.au>
2025-04-15 20:59:24 +1000

    Updated init.py to include models

--- ganblr/models/__init__.py
+++ ganblr/models/__init__.py
@@ -1,5 +1,22 @@
-"""The GANBLR models."""
-from .ganblr import GANBLR
-from .ganblrpp import GANBLRPP
-from .ganblrmug import GANBLR_MUG
-__all__ = ["GANBLR", "GANBLRPP", "GANBLR_MUG"]
+"""Top-level package for Ganblr."""
+
+__author__ = """Tulip Lab"""
+__email__ = 'jhzhou@tuliplab.academy'
+__version__ = '0.1.1'
+
+from .kdb import KdbHighOrderFeatureEncoder
+from .utils import get_demo_data
+
+# The GANBLR models
+from .models.ganblr import GANBLR
+from .models.ganblrpp import GANBLRPP
+from .models.ganblrmug import GANBLR_MUG
+
+__all__ = [
+    'models',
+    'KdbHighOrderFeatureEncoder',
+    'get_demo_data',
+    'GANBLR',
+    'GANBLRPP',
+    'GANBLR_MUG'
+]
